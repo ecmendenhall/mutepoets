e332f2b849b9830e490e31f1cb191d4e56e75f29
Connor Mendenhall <cmendenhall@8thlight.com>
2021-11-18 18:26:47 -0500

    Load fonts from CDN

--- app/mutepoets/src/index.tsx
+++ app/mutepoets/src/index.tsx
@@ -4,21 +4,10 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import config from './config/dapp'
-import { Helmet } from 'react-helmet'
 import { DAppProvider } from '@usedapp/core'
-
-import '@fontsource/josefin-sans'
-import '@fontsource/montserrat'
-
-import montserrat from '@fontsource/montserrat/files/montserrat-latin-400-normal.woff2'
-import josefinSans from '@fontsource/josefin-sans/files/josefin-sans-latin-400-normal.woff2'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Helmet>
-      <link rel="preload" as="font" href={montserrat} type="font/woff2" />
-      <link rel="preload" as="font" href={josefinSans} type="font/woff2" />
-    </Helmet>
     <DAppProvider config={config}>
       <App />
     </DAppProvider>
